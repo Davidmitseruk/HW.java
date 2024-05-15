01d83b6e17d3349947336c179b5e4e1f682c99e8
Davidmitseruk <davidmitseruk@gmail.com>
2024-05-15 18:36:04 +0200

    okay

---
+++ script.js
@@ -0,0 +1,20 @@
+//1
+const result = 5 + 5 + '5';
+console.log(result);
+console.log(typeof result);
+
+//2
+const email = 'davidmitseruk@gmail.com';
+console.log(email.includes('@'))
+
+//3
+const my = 'My';
+const name = 'name';
+const is = 'is';
+const fullName = my + ' ' + name + ' ' + is + ' ' + 'David';
+console.log(fullName);
+
+//4
+let userName = 'David';
+let payment = '59,99';
+alert(`Thank you ${userName} for buying for ${payment}dollars`);
